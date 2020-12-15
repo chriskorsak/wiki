0098c9fb4e2174820313027b4ccd4f5eca3bdf35
chriskorsak <chris@chriskorsak.com>
2020-12-14 21:57:58 -0800

    implement logic to return search results in a list of links if search substring is contained in title of an entry.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -25,9 +25,26 @@ def entry(request, title):
 
 def search(request):
   if request.method == "POST":
+    #assign searchForm class to form variable
     form = searchForm(request.POST)
     if form.is_valid():
       query = form.cleaned_data["query"]
+      #check to see if search query is an esisting entry
       if util.get_entry(query):
+        #redirect to entry url if so
         return redirect("/" + query)
-  return render(request, "encyclopedia/test.html")
+      #return list of pages if substring is in entry, or error page
+      else:
+        #empty list, will be populated if substring in entry
+        partialResults = []
+        entries = util.list_entries()
+        query = query.lower()
+
+        for entry in entries:
+          entry = entry.lower()
+          if query in entry:
+            partialResults.append(entry)
+        return render(request, "encyclopedia/search.html", {
+          "partialResults": partialResults
+        })
+      return render(request, "encyclopedia/error.html")
