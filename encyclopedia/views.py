a126822fb5e9ec0a0b41e3c60c4f8a58b39ec98d
chriskorsak <chris@chriskorsak.com>
2020-12-14 19:23:58 -0800

    add logic for first search spec: If the query matches the name of an encyclopedia entry, the user should be redirected to that entry’s page.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-
+from django import forms
 from . import util
 
+class searchForm(forms.Form):
+    # here you define all the form inputs you want the user to fill out:
+    # CharField is a text input with a label of 'query'
+    query = forms.CharField()
 
 def index(request):
   return render(request, "encyclopedia/index.html", {
@@ -18,3 +22,12 @@ def entry(request, title):
     })
   else:
     return render(request, "encyclopedia/error.html")
+
+def search(request):
+  if request.method == "POST":
+    form = searchForm(request.POST)
+    if form.is_valid():
+      query = form.cleaned_data["query"]
+      if util.get_entry(query):
+        return redirect("/" + query)
+  return render(request, "encyclopedia/test.html")
